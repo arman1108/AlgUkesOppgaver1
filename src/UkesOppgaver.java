fddad134b1cb71fe436675b7bd515a1c2f64ead3
arman1108 <70656307+arman1108@users.noreply.github.com>
2021-08-24 16:06:53 +0200

    1.1.2 gjort

--- src/UkesOppgaver.java
+++ src/UkesOppgaver.java
@@ -1,7 +1,31 @@
 public class UkesOppgaver {
 
-    public static void main(String[] args) {
-        System.out.print("Hei AlgDat");
+
+    public static int min(int[] a) {
+
+
+
+        if (a.length < 1) {
+            throw new java.util.NoSuchElementException("Tabellen a er tom!");
+        }
+
+        int m = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[m] > a[i]) {
+                m = i;
+            }
+        }
+        return m;
     }
+
+    public static void main(String[] args) {
+
+        int[] liste = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
+        System.out.println(min(liste));
+
+    }
+
+
 
 }
